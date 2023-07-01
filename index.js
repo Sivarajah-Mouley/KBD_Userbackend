3c2fdee79d49a456acb2139f73af2e0b4e5ca2ce
Sampavi Sivakumaran <sivasampavi99@gmail.com>
2023-07-01 14:52:15 +0530

    Setup Express&MongoDB

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express= require('express');
+const app=express();
+const cors =require("cors")
+const connection =require("./db")
+
+//database connection
+connection();
+
+//middlewares
+app.use(expree.json())
+app.use(cors());
+
+const port =process.env.PORT|| 3000;
+app.listen(port, () => console.log('listening on port $(port)...'))
