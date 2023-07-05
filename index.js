af28e44c42fc7e1e5f554e5c07232abb8e34a2fe
Sampavi Sivakumaran <sivasampavi99@gmail.com>
2023-07-05 08:57:38 +0530

    Import Routes

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const express= require('express');
 const app=express();
 const cors =require("cors")
 const connection =require("./db")
-
+const userRoutes =require("./routes/users");
+const authRoutes =require("./routes/auth");
 //database connection
 connection();
 
 //middlewares
 app.use(expree.json())
 app.use(cors());
+
+//routes
+app.use("/api/users",userRoutes);
+app.use("/api/auth",authRoutes);
 
 const port =process.env.PORT|| 3000;
 app.listen(port, () => console.log('listening on port $(port)...'))
