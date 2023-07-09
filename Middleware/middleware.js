8b2aa3b841a7a35ccdda446a2402bd793dbc3803
Sampavi Sivakumaran <sivasampavi99@gmail.com>
2023-07-09 14:50:18 +0530

    Modifications of Middleware

--- Middleware/middleware.js
+++ Middleware/middleware.js
@@ -1,5 +1,5 @@
 const User = require('../models/User')
-const {verifyToken} = require('../Helpers/Helper')
+const {verifyToken} = require('../Helpers/helper')
 
 const sendResponseError = (statusCode, msg, res) => {
   res.status(statusCode || 400).send(!!msg ? msg : 'Input is invalid!!')
